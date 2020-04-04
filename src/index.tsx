5e956de799756ba36560c3b46aef37c5f83fa586
huyinglin <fazheng@indata.cc>
2020-04-04 21:27:37 +0800

    feat: add base style config

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      faz
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
