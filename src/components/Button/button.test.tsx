36e700367b69220acc127d49c3468981b04cdfd9
huyinglin <fazheng@indata.cc>
2020-04-05 22:06:29 +0800

    feat: add button component test

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Button from './button';
 
-test('our first react test case', () => {
-  const wrapper = render(<Button>Nice</Button>);
-  const element = wrapper.queryByText('Nice');
-  expect(element).toBeTruthy()
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    const wrapper = render(<Button>Nice</Button>);
+    const element = wrapper.getByText('Nice');
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual('BUTTON');
+    expect(element).toHaveClass('btn btn-default');
+  })
 })
